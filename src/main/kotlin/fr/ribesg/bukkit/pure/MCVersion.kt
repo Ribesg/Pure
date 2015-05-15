2496feb0cd442fc2d88514a857b12186a02189fa
Ribesg <Ribesg@yahoo.fr>
2015-05-15 12:40:51 +0200

    Update libs matching recent changes in relocation process

--- src/main/kotlin/fr/ribesg/bukkit/pure/MCVersion.kt
+++ src/main/kotlin/fr/ribesg/bukkit/pure/MCVersion.kt
@@ -20,7 +20,7 @@ enum class MCVersion {
      */
     A0_2_8 : MCVersion(
         "29BB180262250235131FB53F12D61F438D008620E1B428E372DBADEC731F5E09",
-        "0EA7FFC289665094C4752CF5D369EEB6063B434F32060FE054078166F0D0D3FC",
+        "EBE3CF77CA4E7E3BE45A8B319AB9FE27CD51EEF734DD5CC16C763EE7A91526E8",
         "http://files.ribesg.fr/minecraft_server.jar/minecraft_server.a0.2.8.jar",
         javaClass<fr.ribesg.bukkit.pure.minecraft.a0_2_8.ProxyChunkGenerator>()
     )
@@ -30,7 +30,7 @@ enum class MCVersion {
      */
     R1_2_5 : MCVersion(
         "19285D7D16AEE740F5A0584F0D80A4940F273A97F5A3EAF251FC1C6C3F2982D1",
-        "6673133AB6763B7633B9CE9AA95A1A12E61469A12788794B2544DB79B22A439D",
+        "11BDC22C2CF6785774B09A29BE5743BC9597DE9343BE20262B49C0AC00979D72",
         "https://s3.amazonaws.com/Minecraft.Download/versions/1.2.5/minecraft_server.1.2.5.jar",
         null
     )
@@ -40,7 +40,7 @@ enum class MCVersion {
      */
     R1_3_2 : MCVersion(
         "0795E098D970B459832750D4C6C2C4F58EF55A333A67281418318275E6026EBA",
-        "18CDFDDADD69C1AE962BE49E98E2B57EDA5A6B497D919E5C5B1400B72BC966CF",
+        "0C68314039E8A55B0ACFBDC10B69A1E5B116229CC255B78BD1EF04F0FE4CA6DE",
         "https://s3.amazonaws.com/Minecraft.Download/versions/1.3.2/minecraft_server.1.3.2.jar",
         null
     )
@@ -50,7 +50,7 @@ enum class MCVersion {
      */
     R1_4_7 : MCVersion(
         "96B7512AEAD2FB20DDF780D7DD74208D77F209E16058EA8944150179E65B4DD3",
-        "1A7B2AFC5D3A2EEC67D00ACC4FE25B510CFF8B807AF6C76441FD2B9324871D68",
+        "0A9F5D2CAF06C85A383772BE2C01E6289690555483AC3BAB1DD2F78F75D124C6",
         "https://s3.amazonaws.com/Minecraft.Download/versions/1.4.7/minecraft_server.1.4.7.jar",
         null
     )
@@ -60,7 +60,7 @@ enum class MCVersion {
      */
     R1_5_2 : MCVersion(
         "4F0C7B79CA2B10716703436550F75FA14E784B999707FFAD0EA4E9C38CC256A0",
-        "46E9389D3DE112F0AA843E076DECEFDB0179AADFC1B6DD48F2612D9458F52BEE",
+        "9AA1E33A276EC2EAC6E377679EDF1F9728E10CDBCD01EE91FFECCA773E13C13D",
         "https://s3.amazonaws.com/Minecraft.Download/versions/1.5.2/minecraft_server.1.5.2.jar",
         null
     )
@@ -70,7 +70,7 @@ enum class MCVersion {
      */
     R1_6_4 : MCVersion(
         "81841A2FEDFE0CE19983156A06FA5294335284BEEB95C8CA872D3C1A5FCF5774",
-        "F551DAAAB723E6F2E5EFC940660121179657E061948E177507AFACD723F789CD",
+        "B5F598584EE44B592D35856332495850B6083CE11F4F6928B4F078E14F23FB53",
         "https://s3.amazonaws.com/Minecraft.Download/versions/1.6.4/minecraft_server.1.6.4.jar",
         null
     )
@@ -80,7 +80,7 @@ enum class MCVersion {
      */
     R1_7_10 : MCVersion(
         "C70870F00C4024D829E154F7E5F4E885B02DD87991726A3308D81F513972F3FC",
-        "1BA7248AC5A9ACA1F8C6E3CA8FCBAC5190FA47A70832E3CE2EBBFE53B88BB0E9",
+        "AC886902C6357289ED76D651F03380ABC1835EFFB6953058202191A1E2BAC9DC",
         "http://s3.amazonaws.com/Minecraft.Download/versions/1.7.10/minecraft_server.1.7.10.jar",
         javaClass<fr.ribesg.bukkit.pure.minecraft.r1_7_10.ProxyChunkGenerator>()
     )
@@ -90,7 +90,7 @@ enum class MCVersion {
      */
     R1_8 : MCVersion(
         "40E23F3823D6F0E3CBADC491CEDB55B8BA53F8AB516B68182DDD1536BABEB291",
-        "1CA14C140603FC93D9202CA90D13F0BD216E4F1183084F890E8E23156560FD47",
+        "950C597411A970CC3FCC59E3B04EDE6FCA78BB07D542BD56F077C85E9D45B0B8",
         "http://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar",
         javaClass<fr.ribesg.bukkit.pure.minecraft.r1_8.ProxyChunkGenerator>()
     )
@@ -186,6 +186,6 @@ enum class MCVersion {
     } catch (e: ReflectiveOperationException) {
         throw RuntimeException("Failed to call associated proxy ChunkGenerator class constructor")
     } catch (e: NullPointerException) {
-        throw IllegalStateException("Generator for Minecraft version " + this + " not implemented yet.", e)
+        throw IllegalStateException("Generator for Minecraft version " + this + " is not implemented yet.", e)
     }
 }
