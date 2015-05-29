c82f5a021ae8b8469bf0fcfb9e0e74b9856dded0
Ribesg <ribesg@yahoo.fr>
2015-05-29 17:06:36 +0200

    Use new Kotlin awesomeness

--- src/main/kotlin/fr/ribesg/minecraft/pure/common/Log.kt
+++ src/main/kotlin/fr/ribesg/minecraft/pure/common/Log.kt
@@ -1,5 +1,6 @@
 package fr.ribesg.minecraft.pure.common
 
+import kotlin.jvm.jvmOverloads as overloaded
 import kotlin.platform.platformStatic as static
 
 /**
@@ -37,51 +38,39 @@ public object Log {
         this.slf4jLogger = null
     }
 
-    public static fun debug(msg: String): Unit
-        = this.log(Level.DEBUG, msg)
-
-    public static fun debug(msg: String, t: Throwable): Unit
+    public static overloaded fun debug(msg: String, t: Throwable? = null): Unit
         = this.log(Level.DEBUG, msg, t)
 
-    public static fun info(msg: String): Unit
-        = this.log(Level.INFO, msg)
-
-    public static fun info(msg: String, t: Throwable): Unit
+    public static overloaded fun info(msg: String, t: Throwable? = null): Unit
         = this.log(Level.INFO, msg, t)
 
-    public static fun warn(msg: String): Unit
-        = this.log(Level.WARN, msg)
-
-    public static fun warn(msg: String, t: Throwable): Unit
+    public static overloaded fun warn(msg: String, t: Throwable? = null): Unit
         = this.log(Level.WARN, msg, t)
 
-    public static fun error(msg: String): Unit
-        = this.log(Level.ERROR, msg)
-
-    public static fun error(msg: String, t: Throwable): Unit
+    public static overloaded fun error(msg: String, t: Throwable? = null): Unit
         = this.log(Level.ERROR, msg, t)
 
-    private fun log(level: Level, msg: String) {
-        if (this.javaLogger != null) {
-            this.javaLogger?.log(this.toJavaLoggerLevel(level), msg)
-        } else if (this.slf4jLogger != null) when (level) {
-            Level.DEBUG -> this.slf4jLogger?.debug(msg)
-            Level.INFO  -> this.slf4jLogger?.info(msg)
-            Level.WARN  -> this.slf4jLogger?.warn(msg)
-            Level.ERROR -> this.slf4jLogger?.error(msg)
-            else        -> throw UnsupportedOperationException("Logging Level $level unsupported!")
-        }
-    }
-
-    private fun log(level: Level, msg: String, t: Throwable) {
-        if (this.javaLogger != null) {
-            this.javaLogger?.log(this.toJavaLoggerLevel(level), msg, t)
-        } else if (this.slf4jLogger != null) when (level) {
-            Level.DEBUG -> this.slf4jLogger?.debug(msg, t)
-            Level.INFO  -> this.slf4jLogger?.info(msg, t)
-            Level.WARN  -> this.slf4jLogger?.warn(msg, t)
-            Level.ERROR -> this.slf4jLogger?.error(msg, t)
-            else        -> throw UnsupportedOperationException("Logging Level $level unsupported!")
+    private fun log(level: Level, msg: String, t: Throwable?) {
+        if (t == null) {
+            if (this.javaLogger != null) {
+                this.javaLogger?.log(this.toJavaLoggerLevel(level), msg)
+            } else if (this.slf4jLogger != null) when (level) {
+                Level.DEBUG -> this.slf4jLogger?.debug(msg)
+                Level.INFO  -> this.slf4jLogger?.info(msg)
+                Level.WARN  -> this.slf4jLogger?.warn(msg)
+                Level.ERROR -> this.slf4jLogger?.error(msg)
+                else        -> throw UnsupportedOperationException("Logging Level $level unsupported!")
+            }
+        } else {
+            if (this.javaLogger != null) {
+                this.javaLogger?.log(this.toJavaLoggerLevel(level), msg, t)
+            } else if (this.slf4jLogger != null) when (level) {
+                Level.DEBUG -> this.slf4jLogger?.debug(msg, t)
+                Level.INFO  -> this.slf4jLogger?.info(msg, t)
+                Level.WARN  -> this.slf4jLogger?.warn(msg, t)
+                Level.ERROR -> this.slf4jLogger?.error(msg, t)
+                else        -> throw UnsupportedOperationException("Logging Level $level unsupported!")
+            }
         }
     }
 
