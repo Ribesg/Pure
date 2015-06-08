58b6bdfe3c23097aa030c53cd22d2e960a0df5c6
Ribesg <ribesg@yahoo.fr>
2015-06-08 11:01:15 +0200

    Simplify Spange logger initialization

--- src/main/kotlin/fr/ribesg/minecraft/pure/sponge/Pure.kt
+++ src/main/kotlin/fr/ribesg/minecraft/pure/sponge/Pure.kt
@@ -4,10 +4,7 @@ import com.google.inject.Inject
 import fr.ribesg.minecraft.pure.PomData
 import fr.ribesg.minecraft.pure.common.Log
 import org.slf4j.Logger
-import org.spongepowered.api.event.Subscribe
-import org.spongepowered.api.event.state.PreInitializationEvent
 import org.spongepowered.api.plugin.Plugin
-import kotlin.properties.Delegates
 
 /**
  * @author Ribesg
@@ -15,10 +12,8 @@ import kotlin.properties.Delegates
 Plugin(id = PomData.ARTIFACT_ID, name = PomData.NAME, version = PomData.VERSION)
 public object Pure {
 
-    Inject private val logger: Logger by Delegates.notNull()
-
-    Subscribe fun onPreInit(event: PreInitializationEvent) {
-        Log.initSlf4jLogger(this.logger)
+    Inject private fun setupLogger(logger: Logger) {
+        Log.initSlf4jLogger(logger)
     }
 
     // TODO Everything
